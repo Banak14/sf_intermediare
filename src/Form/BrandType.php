d7512b5a3f18ace5cb5baa0e6e00d67c7ba16d68
Kourouma Banawata <banawata@MacBook-Pro-de-Kourouma.home>
2022-01-12 15:22:46 +0100

    Product, Category, Brand

--- src/Form/BrandType.php
+++ src/Form/BrandType.php
@@ -6,6 +6,7 @@ use App\Entity\Brand;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class BrandType extends AbstractType
